23265a3c6cee5e3aef620d61ba8db39db4027cd8
Ricardo Lozano <ricardo.lozano.quesada@gmail.com>
2024-11-25 23:41:17 +0100

    first commit

---
+++ include/netcommon.h
@@ -0,0 +1,52 @@
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+//pragma ignored by mingw, linking setup required
+#pragma comment(lib, "ws2_32.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+void startWinsock() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+}
+
+void stopWinsock() {
+    WSACleanup();
+}
+
+void socketsSetup() {
+    atexit(stopWinsock);
+    startWinsock();
+}
+#define INIT_SOCKETS() socketsSetup();
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+void socketsSetup() {
+    return;
+}
+#endif
+
+#define INIT_SOCKETS() socketsSetup();
